aabbd392c955564f9a67b0d002b8411b64ffbd3e
codersyntax <codersyntaxdev@gmail.com>
2021-12-27 22:41:05 -0500

    change to tprint

--- contractor.js
+++ contractor.js
@@ -3,15 +3,15 @@ import { getNsDataThroughFile, disableLogs, scanAllServers } from './helpers.js'
 /** @param {NS} ns **/
 export async function main(ns) {
     disableLogs(ns, ["scan"]);
-    ns.print("Getting server list...");
+    ns.tprint("Getting server list...");
     const servers = scanAllServers(ns);
-    ns.print(`Got ${servers.length} servers. Searching for contracts on each...`);
+    ns.tprint(`Got ${servers.length} servers. Searching for contracts on each...`);
     const contractsDb = servers.map(hostname => ({ hostname, contracts: ns.ls(hostname, '.cct') }))
         .filter(o => o.contracts.length > 0)
         .map(o => o.contracts.map(contract => ({ contract, hostname: o.hostname }))).flat();
     if (contractsDb.length == 0)
         return ns.print("Found no contracts to solve.");
-    ns.print(`Found ${contractsDb.length} contracts to solve. Gathering contract data via separate scripts..."`);
+    ns.tprint(`Found ${contractsDb.length} contracts to solve. Gathering contract data via separate scripts..."`);
     let contractsDictCommand = command => `Object.fromEntries(${JSON.stringify(contractsDb)}.map(c => [c.contract, ${command}]))`;
     let dictContractTypes = await getNsDataThroughFile(ns, contractsDictCommand('ns.codingcontract.getContractType(c.contract, c.hostname)'), '/Temp/contract-types.txt');
     let dictContractData = await getNsDataThroughFile(ns, contractsDictCommand('ns.codingcontract.getData(c.contract, c.hostname)'), '/Temp/contract-data.txt');
